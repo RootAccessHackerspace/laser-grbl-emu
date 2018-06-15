38a212d5e2ae8280725bb1144c8916ef84334654
Dylan Armitage <dylanjarmitage@gmail.com>
2018-06-15 14:59:57 -0700

    [add] begin GRBL inpretation

--- fake_serial.py
+++ fake_serial.py
@@ -18,6 +18,58 @@ import coloredlogs
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+#### GRBL responses ##########################################################
+OK = 'ok'
+SETTING_DISABLED = 'error:Setting disabled'
+NOT_IDLE = 'error:Not idle'
+ALARM_LOCK = 'error:Alarm lock'
+LINE_OVERFLOW = 'error:Line overflow'
+MODAL_VIOLATION = 'error:Modal group violation'
+UNSUPPORTED_COMMAND = 'error:Unsupported command'
+UNDEFINED_FEED = 'error:Undefined feed rate'
+INVALID_GCODE = {'not integer': 'error:Invalid gcode ID:23',
+                 'both xyz': 'error:Invalid gcode ID:24',
+                 'repeated': 'error:Invalid gcode ID:25',
+                 'no xyz': 'error:Invalid gcode ID:26',
+                 'nline oor': 'error:Invalid gcode ID:27',
+                 'missing pl': 'error:Invalid gcode ID:28',
+                 'unsup wcs': 'error:Invalid gcode ID:29',
+                 'wrong g53 mode': 'error:Invalid gcode ID:30',
+                 'unused axis': 'error:Invalid gcode ID:31',
+                 'no xyz arc': 'error:Invalid gcode ID:32',
+                 'invalid target': 'error:Invalid gcode ID:33',
+                 'arc geom rad': 'error:Invalid gcode ID:34',
+                 'arc miss IJK': 'error:Invalid gcode ID:35',
+                 'unused words': 'error:Invalid gcode ID:36',
+                 'tool offset axis': 'error:Invalid gcode ID:37'
+                }
+ALARM_HLIM = 'ALARM:Hard limit'
+ALARM_SLIM = 'ALARM:Soft limit'
+ALARM_ABORT = 'ALARM:Abort during cycle'
+RESET_CONTINUE = '[reset to continue]'
+TO_UNLOCK = '[\'$H\'|\'$X\' to unlock]'
+CAUTION = '[Caution: Unlocked]'
+ENABLED = '[Enabled]'
+DISABLED = '[Disabled]'
+IDLE = 'Idle'
+RUNNING = 'Run'
+ALARM = 'ALARM'
+
+class grbl_interpret:
+    '''A simple python implementation of grbl
+
+    Mostly just makes sure valid gcode is being used
+    '''
+    def __init__(self):
+        self.modal_state = ['G0', 'G54', 'G17', 'G21', 'G90', 'G94', 'M0', 'M5', 'M9', 'T0', 'F0.', 'S0.']
+        self.state_pos = {'state': IDLE,
+                          'mpos': [0.000, 0.000, 0.000],
+                          'wpos': [0.000, 0.000, 0.000],
+                          'plan buf': 0,
+                          'rx buf': 0
+                         }
+
+
 class Serial:
     
     def __init__(self, port='/dev/ttyAMA0', baudrate=115200, timeout=1, bytesize=8,
